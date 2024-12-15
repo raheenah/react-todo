7eb1bcf3029bd03fd01d5713cdc3d05e833c2d1d
Mohammed Raheenat Sunmisola <raheenah500@gmail.com>
2024-12-15 16:55:09 +0100

    Active states on nav enabled

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -23,29 +23,47 @@ const Footer = () => {
       // icon: "<i className="fa- brands fa - instagram"></i>",
       username: "Raheenat Mohammed",
     },
-
+    {
+      id: "github",
+      icon: "fa-brands fa-github",
+      link: "https://www.github.com/raheenah",
+      // icon: "<i className="fa- brands fa - instagram"></i>",
+      username: "Raheenat Mohammed",
+    },
   ];
 
   return (
     <div className='bg-background pb-8 px-8'>
       {" "}
-      <div className='py-4 flex flex-col gap-2 lg:max-w-[80%] w-full justify-center items-center mx-auto  shadow-custom-todo rounded-lg px-2'>
+      <div className='py-4 flex flex-col gap-2 lg:max-w-[80%] w-full justify-center items-center mx-auto  shadow-custom-todo rounded-lg px-4'>
         <h3 className='text-text-primary font-bold text-lg'>
           Say Hi… or Stay Shy
         </h3>
-        <div className='flex md:w-[60%] w-full justify-between items-center mt-6  '>
-          <ul className='flex gap-4 '>
-            {FooterData.map((item) => (
-              <li key={item.id} className='flex'>
-                {/* <div className='bg-blue-300 flex items-center justify-center relative w-full h-full'> */}
-                <NavLink to={item.link} className='text-button-bg hover:text-button-hover text-lg '>
-                  <i className={item.icon}></i>
-                </NavLink>
-                {/* </div> */}
-              </li>
-            ))}
-          </ul>
-          <p className='text-md text-text-primary font-semibold'>© Raheenah 2024</p>
+        <div className='flex md:w-[60%] w-full justify-between items-end mt-6  '>
+          <div className='flex flex-col gap-4'>
+            <ul className='flex gap-4 '>
+              {FooterData.map((item) => (
+                <li key={item.id} className='flex'>
+                  {/* <div className='bg-blue-300 flex items-center justify-center relative w-full h-full'> */}
+                  <NavLink
+                    to={item.link}
+                    className='text-button-bg hover:text-button-hover text-lg '
+                  >
+                    <i className={item.icon}></i>
+                  </NavLink>
+                  {/* </div> */}
+                </li>
+              ))}
+            </ul>{" "}
+            <div className='flex gap-2 items-center text-text-primary'>
+              {" "}
+              <i className='fa-solid fa-location-dot primary'></i>
+              <p>Abuja, Nigeria</p>
+            </div>
+          </div>
+          <p className='text-sm text-text-primary font-light'>
+            © Raheenah 2024
+          </p>
         </div>
       </div>
     </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,48 @@
-import { NavLink } from "react-router-dom"
+import { NavLink, useLocation } from "react-router-dom"
 
 const NavBar = () => {
+  const location = useLocation();
+
+  const isActive = (path) => location.pathname === path;
+
     return (
       <div className='bg-background fixed w-full py-8 z-50 px-8 mx-auto'>
         <ul className='flex py-2 gap-2  text-center lg:max-w-[80%] w-full mx-auto  items-center justify-center  shadow-custom-todo rounded-lg px-2'>
-          <li className='text-button-bg hover:text-button-hover hover:underline hover:font-bold font-semibold'>
-            <NavLink to='/'>Home</NavLink>
+          <li>
+            <NavLink
+              to='/'
+              className={`text-button-bg hover:text-button-hover hover:underline hover:font-bold font-semibold ${
+                isActive("/") ? "text-button-hover underline" : "text-button-bg"
+              }`}
+            >
+              Home
+            </NavLink>
           </li>
           <li className='text-button-bg'>|</li>
           <li className='text-button-bg hover:text-button-hover hover:underline hover:font-bold font-semibold'>
-            <NavLink to='/error-boundry-page'>Err Boundary</NavLink>
+            <NavLink
+              to='/error-boundry-page'
+              className={`text-button-bg hover:text-button-hover hover:underline hover:font-bold font-semibold ${
+                isActive("/error-boundry-page")
+                  ? "text-button-hover underline"
+                  : "text-button-bg"
+              }`}
+            >
+              Err Boundary
+            </NavLink>
           </li>
           <li className='text-button-bg'>|</li>
           <li className='text-button-bg hover:text-button-hover hover:underline hover:font-bold font-semibold'>
-            <NavLink to='go-away'>Not Found </NavLink>
+            <NavLink
+              to='go-away'
+              className={`text-button-bg hover:text-button-hover hover:underline hover:font-bold font-semibold ${
+                isActive("/go-away")
+                  ? "text-button-hover underline"
+                  : "text-button-bg"
+              }`}
+            >
+              Not Found{" "}
+            </NavLink>
           </li>
         </ul>
       </div>
